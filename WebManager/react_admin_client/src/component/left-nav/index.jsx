18da6912602e6c1004ac7bbcf1bd956d9adb4ef4
hibiscus <zxt502838517@gmail.com>
2020-04-08 21:45:15 +0800

    添加左侧菜单栏自动选中以及自动打开子目录功能

--- WebManager/react_admin_client/src/component/left-nav/index.jsx
+++ WebManager/react_admin_client/src/component/left-nav/index.jsx
@@ -1,8 +1,7 @@
 import React, {Component} from "react";
-import {Link} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
 import { Menu } from 'antd';
 import * as icons from '@ant-design/icons';
-import Dexie from 'dexie';
 
 import './index.less';
 import logo from '../../assets/images/logo.png';
@@ -24,8 +23,12 @@ const dict = {
     "<MenuOutlined />": <icons.MenuOutlined />,
 }
 
-export default class LeftNav extends Component {
-    getMenuNodes = (MenuConfig) => {
+class LeftNav extends Component {
+    /*
+    * 根据 MenuConfig 生成对应的标签数组
+    * 使用 map + 递归
+    */
+    getMenuNodesMap = (MenuConfig) => {
       return MenuConfig.map(item => {
           if (!item.children) {
               // console.log(item.icon);
@@ -56,11 +59,60 @@ export default class LeftNav extends Component {
       })
     };
 
+    /*
+    * 根据 MenuConfig 生成对应的标签数组
+    * 使用 reduce + 递归
+    */
+    getMenuNodes = (MenuConfig) => {
+        return MenuConfig.reduce((pre, item) => {
+            // 获取当前请求路由路径
+            const currentPath = this.props.location.pathname;
+
+            if (!item.children) {
+                // 向 pre 添加 <Menu.Item>
+                pre.push((
+                    <Menu.Item key={item.key}>
+                        <Link to={item.key}>
+                            {dict[item.icon]}
+                            {/*<PieChartOutlined />*/}
+                            <span>{item.title}</span>
+                        </Link>
+                    </Menu.Item>
+                ))
+            } else {
+                const childrenItem = item.children.find(childrenItem => childrenItem.key===currentPath);
+                if (childrenItem) {
+                    this.openKey = item.key;
+                }
+
+                // 向 pre 添加 <SubMenu>
+                pre.push((
+                    <SubMenu
+                        key={item.key}
+                        title={
+                            <span>
+                                {dict[item.icon]}
+                                <span>{item.title}</span>
+                            </span>
+                        }
+                    >
+                        {this.getMenuNodes(item.children)}
+                    </SubMenu>
+                ))
+            }
+            return pre
+        }, [])
+    }
+
     UNSAFE_componentWillMount () {
         this.menuNodes = this.getMenuNodes(MenuConfig);
     }
 
     render() {
+        // 获取当前请求路由路径
+        const currentPath = this.props.location.pathname;
+        const openKey = this.openKey
+
         return (
             <div className='left-nav'>
                 <Link to='/' className='left-nav-header'>
@@ -71,6 +123,8 @@ export default class LeftNav extends Component {
                 <Menu
                     mode="inline"
                     theme="dark"
+                    selectedKeys={[currentPath]}
+                    defaultOpenKeys={[openKey]}
                 >
                     {this.menuNodes}
                 </Menu>
@@ -78,3 +132,10 @@ export default class LeftNav extends Component {
         )
     }
 }
+
+/*
+* withRouter 高阶组件：
+*   包装非路由组件，返回新组件，新组件向非路由组件传递 3 个属性：
+*   history location match
+*/
+export default withRouter(LeftNav)
